74d1b57868a44d25c3382995058e0064b60c1a06
Asmit Das Joy <asmitjoy17@gmail.com>
2020-02-05 18:34:56 +0600

    Working with Method and Object

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+    class Calculator{
+        function add($x,$y){
+            echo"Sumation = ".($x+$y)."<br>";
+        }
+        function sub($x,$y){
+            echo"Subtraction  = ".($x-$y)."<br>";
+        }
+        
+        function multi($x,$y){
+            echo"Multiplication = ".($x*$y)."<br>";
+        }        
+        function div($x,$y){
+            echo"Division = ".($x/$y)."<br>";
+        }
+    }
+
+
+
+
+
+?>
