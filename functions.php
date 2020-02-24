6b9dfffb997a46e9e743f22ed0dcc23db27a5b97
Asmit Das Joy <asmitjoy17@gmail.com>
2020-02-24 19:30:35 +0600

    Method-chaining

--- functions.php
+++ functions.php
@@ -15,6 +15,23 @@
             echo"Division = ".($x/$y)."<br>";
         }
     }
+    class Calculation_2{
+
+        public $a=0;
+        public $b=0;
+        public $result;
+
+        public function getValue($x,$y)
+        {
+            $this->a = $x;
+            $this->b = $y; 
+            return $this;
+        }
+        public function getResult(){
+            $this->result = ($this->a+$this->b); 
+            return $this->result;
+        }
+    }
 
 
 
