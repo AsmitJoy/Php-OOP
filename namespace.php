406827e24ce2c0a7e2b29ab2bb5fa4c03186ec29
Asmit Das Joy <asmitjoy17@gmail.com>
2020-03-29 17:17:18 +0600

    namespace

---
+++ namespace.php
@@ -0,0 +1,10 @@
+<?php
+namespace joy;
+    class coding{
+        function __construct(){
+            echo"I love Ronaldo<br>";
+        }
+    }
+    define("Html","I love html a little.<br>");
+    const CSS = "I love CSS<br>";
+?>
